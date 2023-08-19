4e7f6fb10aba2f4dd9e06cb8a4aea8ff3997d8ea
Amanda Freitas <avsdfreitas@gmail.com>
2023-08-19 15:22:19 -0300

    Indentação

--- makeIt/index.php
+++ makeIt/index.php
@@ -23,40 +23,38 @@
 
     <article class="etapas">
             <p>Etapas do Desenvolvimento
-Configuração Inicial:
+        Configuração Inicial:
 
-Configurar um ambiente local de desenvolvimento (por exemplo, usando o XAMPP ou MAMP).
-Criar a estrutura básica do projeto (diretórios para HTML, CSS, JavaScript, PHP).
-Estabelecer conexão com o banco de dados MySQL.
-Sistema de Usuários:
+        Configurar um ambiente local de desenvolvimento (por exemplo, usando o XAMPP ou MAMP).
+        Criar a estrutura básica do projeto (diretórios para HTML, CSS, JavaScript, PHP).
+        Estabelecer conexão com o banco de dados MySQL.
+        Sistema de Usuários:
 
-Criar a tabela de usuários no MySQL.
-Desenvolver o sistema de registro e login de usuários.
-Implementar a segurança necessária (por exemplo, hashing de senhas).
-Dashboard:
+        Criar a tabela de usuários no MySQL.
+        Desenvolver o sistema de registro e login de usuários.
+        Implementar a segurança necessária (por exemplo, hashing de senhas).
+        Dashboard:
 
-Criar a interface principal após o login, onde o usuário pode ver seus projetos e eventos.
-Permitir a criação de novos projetos e eventos.
-Funcionalidades de Projeto:
+        Criar a interface principal após o login, onde o usuário pode ver seus projetos e eventos.
+        Permitir a criação de novos projetos e eventos.
+        Funcionalidades de Projeto:
 
-Criar a tabela de projetos e tarefas no MySQL.
-Desenvolver a interface para adicionar/editar/deletar projetos.
-Implementar a funcionalidade de subtópico-todolist dentro de cada projeto.
-Adicionar a área de discussão para cada tarefa.
-Integrações:
+        Criar a tabela de projetos e tarefas no MySQL.
+        Desenvolver a interface para adicionar/editar/deletar projetos.
+        Implementar a funcionalidade de subtópico-todolist dentro de cada projeto.
+        Adicionar a área de discussão para cada tarefa.
+        Integrações:
 
-Integrar com a API do Google Agenda para adicionar eventos.
-Implementar notificações por e-mail para alterações no projeto e notificações de eventos.
-Estilização e Design:
+        Integrar com a API do Google Agenda para adicionar eventos.
+        Implementar notificações por e-mail para alterações no projeto e notificações de eventos.
+        Estilização e Design:
 
-Estilizar o projeto de forma semelhante ao Basecamp da 37signals.
-Refinar a experiência do usuário (UX).
-JavaScript:
+        Estilizar o projeto de forma semelhante ao Basecamp da 37signals.
+        Refinar a experiência do usuário (UX).
+        JavaScript:
 
-Implementar a interatividade na interface usando JavaScript. Como você mencionou que gostaria de aprender mais sobre isso, podemos usar exemplos simples e ir aumentando a complexidade conforme você se sinta confortável.</p>
+        Implementar a interatividade na interface usando JavaScript. Como você mencionou que gostaria de aprender mais sobre isso, podemos usar exemplos simples e ir aumentando a complexidade conforme você se sinta confortável.</p>
     </article>
-
-
     <main>
     <div class="container">
 
