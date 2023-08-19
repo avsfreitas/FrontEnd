fa71b49eddfd5b2534ca9389c981326cc02546ad
unknown <Tramo-TI@DESKTOP-H13KVBT.TRAMO.local>
2023-08-19 14:46:56 -0300

    Meu Primeiro Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfólio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+        <nav>
+
+            <ul>
+
+                <li><a href="">Pisc</a></li>
+                <li><a href="makeIt/">Gerenciador de projetos</a></li>
+                <li><a href="">L2Dragon</a></li>
+            </ul>
+
+        </nav>
+
+
+</body>
+</html>
---
+++ makeIt/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>makeIt</title>
+    <script src="https://kit.fontawesome.com/f5de20681c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+        <h1>MakeIt <i class="fa-solid fa-pen" style="color: #ffffff;"></i> <span> Gerencie seus Projetos </span></h1>
+        
+            <ul>
+                <li>Home</li>
+                <li>Sobre Nós</li>
+                <li>Serviços</li>
+                <li>Acessar Painel</li>
+            </ul>
+        </nav>
+    </header>
+
+    <article class="etapas">
+            <p>Etapas do Desenvolvimento
+Configuração Inicial:
+
+Configurar um ambiente local de desenvolvimento (por exemplo, usando o XAMPP ou MAMP).
+Criar a estrutura básica do projeto (diretórios para HTML, CSS, JavaScript, PHP).
+Estabelecer conexão com o banco de dados MySQL.
+Sistema de Usuários:
+
+Criar a tabela de usuários no MySQL.
+Desenvolver o sistema de registro e login de usuários.
+Implementar a segurança necessária (por exemplo, hashing de senhas).
+Dashboard:
+
+Criar a interface principal após o login, onde o usuário pode ver seus projetos e eventos.
+Permitir a criação de novos projetos e eventos.
+Funcionalidades de Projeto:
+
+Criar a tabela de projetos e tarefas no MySQL.
+Desenvolver a interface para adicionar/editar/deletar projetos.
+Implementar a funcionalidade de subtópico-todolist dentro de cada projeto.
+Adicionar a área de discussão para cada tarefa.
+Integrações:
+
+Integrar com a API do Google Agenda para adicionar eventos.
+Implementar notificações por e-mail para alterações no projeto e notificações de eventos.
+Estilização e Design:
+
+Estilizar o projeto de forma semelhante ao Basecamp da 37signals.
+Refinar a experiência do usuário (UX).
+JavaScript:
+
+Implementar a interatividade na interface usando JavaScript. Como você mencionou que gostaria de aprender mais sobre isso, podemos usar exemplos simples e ir aumentando a complexidade conforme você se sinta confortável.</p>
+    </article>
+
+
+    <main>
+    <div class="container">
+
+    <section id="servicos">
+        <article id="mainCard" class="mainCard">
+            <h2>Crie seu Projeto</h2>
+            <p>Crie seu Projeto, Defina suas Prioridades e Prazos</p>
+        </article>
+
+        <article id="editProject" class="mainCard">
+            <h2>Gerencie seu Projeto</h2>
+            <p>Defina Tarefas, Cargos e até como fazer as atividades</p>
+        </article>
+
+        <article id="viewProject" class="mainCard">
+            <h2>Desenvolvimento do seu Projeto (Power BI ou Excel)</h2>
+            <p>Já pensou tudo bem explicadinho pra apresentar para o Cliente?</p>
+        </article>
+    </section>
+    </div>
+    </main>
+
+    <footer>
+        Dragon Dev. @2023 Todos os Direitos Reservados
+    </footer>
+</body>
+</html>
